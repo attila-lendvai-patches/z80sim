f77d7fe9d7e12a8bd69549bcc18d882c53409b7b
Daniel A. Nagy <nagydani@epointsystem.org>
2025-10-16 22:31:36 +0200

    Add simulator source

---
+++ z80sim.c
@@ -0,0 +1,53 @@
+#include <errno.h>
+#include <z80.h>
+#include <stdio.h>
+
+#define STDIOPORT 0x00
+#define HALT 0x76
+
+static byte memory[ 0x10000 ];
+
+static byte getChar(int param, ushort address) {
+	return ((address & 0xFF) != STDIOPORT) ? 0 : (byte)getchar();
+}
+
+static void putChar(int param, ushort address, byte data) {
+	if ((address & 0xFF) == STDIOPORT) {
+		putchar(data);
+		fflush(stdout);
+	}
+}
+
+static byte getMem(int param, ushort address) {
+	return memory[address];
+}
+
+static void putMem(int param, ushort address, byte data) {
+	memory[address] = data;
+}
+
+static Z80Context context;
+
+int main(int argc, char *argv[]) {
+	FILE *f; int i, c;
+	if(argc !=2) {
+		fprintf(stderr, "usage: %s filename\n", argv[0]);
+		return -1;
+	}
+	if((f = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "%s not found", argv[1]);
+		return errno;
+	}
+	for(i = 0; (i < 0x10000) && ((c = getc(f)) != EOF); i++) memory[i] = (byte)c;
+	while(i < 0x10000) memory[i++] = HALT;
+	fclose(f);
+	context.memRead = getMem;
+	context.memWrite = putMem;
+	context.ioRead = getChar;
+	context.ioWrite = putChar;
+	Z80RESET(&context);
+	while(!context.halted)
+		Z80Execute(&context);
+	fflush(stdout);
+	return 0;
+}
